183777167f088dd7c24c23d59bad201fe8b6cf44
AlexAR <moltron_nano@hotmail.com>
2022-09-06 09:14:12 -0600

    feat: add more implicit testing

--- src/main/scala/lectures/part4implicits/TypeClasses.scala
+++ src/main/scala/lectures/part4implicits/TypeClasses.scala
@@ -45,6 +45,9 @@ object TypeClassesPart2 extends App {
   object HTMLSerializer {
     def serialize[T](value: T)(implicit serializer: HTMLSerializerAll[T]): String =
       serializer.serialize(value)
+
+    //add this apply to have access to the syntax serializer[someType].serialize()
+    def apply[T](implicit serializer: HTMLSerializerAll[T]) = serializer
   }
 
   implicit object IntSerializer extends HTMLSerializerAll[Int] {
@@ -58,5 +61,41 @@ object TypeClassesPart2 extends App {
 
   println(HTMLSerializer.serialize(100))
   println(HTMLSerializer.serialize(someUser))
+
+  println(HTMLSerializer[User].serialize(someUser))
+
+}
+
+object TypeClassPart2Exercise extends App {
+
+  case class User(id: Int, name: String, email: String)
+
+  val someUser = User(1212, "Esther", "esda@gmail.com")
+  val someUser2 = User(1212, "Esther", "esda2@gmail.com")
+  val someUser3 = User(1212, "Esther", "esda@gmail.com")
+  //1) start with a trait
+  trait Equalizer[T] {
+    def euqualize(a: T, b: T) : Boolean
+  }
+  //2) create the singleton that needs to extend the trait also with generics
+  object Equalizer {
+    def euqualize[T](a: T, b: T)(implicit equalizer: Equalizer[T]) : Boolean
+    = equalizer.euqualize(a,b)
+
+    //create apply to have access to the instance approach and have access to al methods
+    def apply[T](implicit equalizer: Equalizer[T]) = equalizer
+  }
+
+  //3) create the concrete implicit implementations
+  implicit object UserEqualizer extends Equalizer[User] {
+    override def euqualize(a: User, b: User): Boolean = {
+      if(a.name == b.name && a.email == b.email) true
+      else false
+    }
+  }
+
+  //4) call using the instance approach
+  println(Equalizer[User].euqualize(someUser,someUser2))
+  println(Equalizer[User].euqualize(someUser,someUser3))
 
 }
